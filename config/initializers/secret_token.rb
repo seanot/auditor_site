0c7b2189ddc6a64433e686c7a89b9dc052cfe008
seanot <sean@kingfieldcapital.com>
2014-04-30 10:50:39 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AuditorSite::Application.config.secret_key_base = '496b02cd66203808e28f5d717f1b9f17e2959f97d20c5ac02a21d60707bc18c5644e863cfcaebde4605a576d1e4ddd86cbe44cb9947afb19dcc449c73c8587fc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AuditorSite::Application.config.session_store :cookie_store, key: '_auditor_site_session'
